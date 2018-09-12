d0bf480f5de8bfc7dd9f029eb3d4c083306c3f0a
Robbie Evans <robbie.evans@Y22-Design-Robbie-Evans-3.local>
2018-09-12 13:40:57 -0400

    Taking Input test 2

---
+++ TakingInput.py
@@ -0,0 +1,9 @@
+#Input
+#Assignment Statement
+
+r = input("What is the radius: ")
+h = input("What is the height: ")
+
+#Process
+
+#Output
---
+++ TakingInputsInt.py
@@ -0,0 +1,11 @@
+#Input
+#Assignment Statement
+
+r = input("What is the radius: ")
+h = input("What is the height: ")
+
+#Process
+
+print("The radius is: "+r*h+"")
+
+#Output
